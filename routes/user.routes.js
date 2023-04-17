18e68a54242c05a33cff5cbe4e443ce9323a032f
Niket Gupta <niketgupta101@gmail.com>
2023-04-17 22:17:09 +0530

    protect user routes

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,11 +1,12 @@
 import * as express from 'express';
 
 import * as userController from '../controller/user.controller.js';
+import { isAuthorized } from '../middleware/isAuthorized.js';
 
 const router = express.Router();
 
-router.get('/', userController.fetchAllUsers);
+router.get('/', isAuthorized, userController.fetchAllUsers);
 router.post('/', userController.createUser);
-router.get('/:userId', userController.fetchUserById);
+router.get('/:userId', isAuthorized, userController.fetchUserById);
 
 export default router;
